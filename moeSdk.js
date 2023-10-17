9050bda4970c0ca3c5040aab9725f30c780487ab
Srimutu <111839903+Srimutu@users.noreply.github.com>
2023-10-17 15:17:47 +0530

    Update moeSdk.js
    Added Soft ask js part

--- moeSdk.js
+++ moeSdk.js
@@ -43,3 +43,10 @@
       debug_logs: 1,
       swPath: "serviceworker.js"
     });
+
+Moengage.call_web_push({
+    "soft_ask":true,
+    "main_class":"moe-main-class",
+    "allow_class":"moe-allow-class",
+    "block_class":"moe-block-class"
+});
